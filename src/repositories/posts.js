df71b7a1eccdf4761d30a28da889d7f0b7973f07
Vijay Rangan <vijay.rangan@gmail.com>
2021-04-24 22:33:40 +0530

    remove comment

--- src/repositories/posts.js
+++ src/repositories/posts.js
@@ -1,7 +1,5 @@
 const database = require("../database");
 
-// can't swap implementation of the persistence layer
-// this makes testing cumbersome
 module.exports = {
   findById(id) {
     return database.first("*").from("posts").where("id", id);
